adfa80cb76b196711f0a9c8dd4896305da91f8b2
nguyendangviet12022004 <viet.ngdang.dev@gmail.com>
2026-02-18 10:15:35 +0700

    feat: Integrate Kong as a Helm dependency, add authentication configuration, and refine Kubernetes resource definitions.

--- k8s/abms/secret.sh
+++
@@ -1,3 +0,0 @@
-
-
-kubectl create secret generic backend-secret --from-literal=DB_USERNAME=root --from-literal=DB_PASSWORD=123456 -n backend --dry-run=client -o yaml
--- k8s/regcred.sh
+++
@@ -1,6 +0,0 @@
-kubectl create secret generic regcred \
-    --from-file=.dockerconfigjson=./credentials.json \
-    --type=kubernetes.io/dockerconfigjson \
-    --namespace=database \
-    --dry-run=client \
-    -o yaml > regcred.yaml
